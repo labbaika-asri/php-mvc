595c9f3c716b2479425e7c1ab4f16f49e7b163c6
Labbaika Asri <labbaikaasri07@gmail.com>
2022-02-04 19:45:49 +0700

    Making controllers

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+    class About
+    {
+        public function index($nama = "Labbaika Asri", $pekerjaan = "Programer")
+        {
+            echo "Hallo nama saya $nama saya adalah seorang $pekerjaan";
+        }
+        public function page()
+        {
+            echo "About/page";
+        }
+    }
--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,38 @@
 <?php
     class App
     {
+        protected $controller = 'Home';
+        protected $method = 'index';
+        protected $params = [];
+
         public function __construct()
         {
             $url = $this->parseURL();
-            var_dump($url);
+
+            // Cek apakah file controller ada
+            if (is_array($url) && file_exists("../app/controllers/" . $url[0] . ".php")) {
+                $this->controller = $url[0];
+                unset($url[0]); // Menghapus url[0]
+            }
+            require_once '../app/controllers/' . $this->controller . '.php';
+            $this->controller = new $this->controller;
+            
+            // method
+            if (isset($url[1])) {
+                // Cek apakah terdapat method
+                if (method_exists($this->controller, $url[1])) {
+                    $this->method = $url[1];
+                    unset($url[1]);
+                }
+            }
+
+            // params
+            if (!empty($url)) {
+                $this->params = array_values($url); // Memindahkan isi dari array url ke property params
+            }
+
+            // Jalankan controller dan method serta kirimkan params jika ada
+            call_user_func_array([$this->controller, $this->method], $this->params);
         }
 
         public function parseURL()
@@ -13,6 +41,7 @@
                 $url = rtrim($_GET["url"], '/'); // Menghapus slash diakhir url
                 $url = filter_var($url, FILTER_SANITIZE_URL); // Membersihkan url dari karakter aneh
                 $url = explode('/', $url); // Pecah url menjadi array
+                var_dump($url);
                 return $url;
             }
         }
