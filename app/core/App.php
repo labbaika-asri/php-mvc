93df5690c2b9382e30124103fbfb89ddd19cec8d
Labbaika Asri <labbaikaasri07@gmail.com>
2022-02-04 16:00:23 +0700

    Adding routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,17 @@
     {
         public function __construct()
         {
-            echo "OK!";
+            $url = $this->parseURL();
+            var_dump($url);
+        }
+
+        public function parseURL()
+        {
+            if (isset($_GET["url"])) {
+                $url = rtrim($_GET["url"], '/'); // Menghapus slash diakhir url
+                $url = filter_var($url, FILTER_SANITIZE_URL); // Membersihkan url dari karakter aneh
+                $url = explode('/', $url); // Pecah url menjadi array
+                return $url;
+            }
         }
     }
